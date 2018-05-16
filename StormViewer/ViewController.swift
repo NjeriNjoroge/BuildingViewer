8a866063a83f02e3e2b947ab7be154f6f6a1d1b5
NjeriNjoroge <wanjorogegrace@gmail.com>
2018-05-16 17:47:55 +0300

    initial commit

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //we'll be using file manager to look for files
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        
+        //the try keyword means the following code has a potential of going wrong but i'm certain it won't
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("unsplash") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
     }
 
     override func didReceiveMemoryWarning() {
